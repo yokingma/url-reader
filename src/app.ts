9c546f00f6a2aa143cd324bfdf6e0159a1434454
zac <yoking@outlook.com>
2024-04-30 18:02:24 +0800

    update: fix type error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import * as Koa from 'koa';
+
 const app = new Koa();
-app.use(async ctx => {
+
+app.use(async (ctx) => {
   ctx.body = 'Hello World';
 });
 
 app.listen(3030);
+
+console.log('[SERVER] started on port 3030.');
